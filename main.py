c56d74a9806a3cecebb60b0d1d861e04bb76dbe9
davideliyahu18-gif <davideliyahu18@gmail.com>
2026-06-18 22:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests
 
 BOT_TOKEN = "8648426108:AAHhM-5ioqeiVPYSDfyVcOgfIl2VL1Q9NdI"
@@ -5,14 +6,12 @@ CHAT_ID = "@eliyahucup"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": "🚀 בדיקת בוט מונדיאל"
-}
-
-r = requests.post(url, data=data)
+r = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "בדיקה"
+    }
+)
 
 print(r.text)
-
-
-
