cdc97cb685e75ec889eb07543c1815b5829e7bff
davideliyahu18-gif <davideliyahu18@gmail.com>
2026-06-18 21:58:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("FIFA BOT WORKING")
+
+import requests
+ 
+BOT_TOKEN ="8648426108:AAHhM-5ioqeiVPYSDfyVcOgfIl2VL1Q9NdI"
+CHAT_ID = "@israelmood"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🚀 בדיקת בוט מונדיאל - החיבור עובד!"
+}
+
+r = requests.post(url, data=data)
+print(r.text)
