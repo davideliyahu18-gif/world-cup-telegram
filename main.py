d8d430276815ba9e2a7c373bb84da95ebafd3f0a
davideliyahu18-gif <davideliyahu18@gmail.com>
2026-06-18 21:50:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-
-import requests
-
-API_KEY = "d1afcf8ed3852939ef24767220889871"
-
-url = "https://v3.football.api-sports.io/fixtures?live=all"
-
-headers = {
-    "x-apisports-key": API_KEY
-}
-
-response = requests.get(url, headers=headers)
-
-print(response.json())
+print("FIFA BOT WORKING")
