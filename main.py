7e2d85a106bf216773af4b8cb1d20f833ca12c5d
davideliyahu18-gif <davideliyahu18@gmail.com>
2026-06-18 22:14:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("BOT STARTED")
+import requests
+
+BOT_TOKEN = "8648426108:AAHhM-5ioqeiVPYSDfyVcOgfIl2VL1Q9NdI"
+CHAT_ID = "@eliyahucup"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🚀 בדיקת בוט מונדיאל"
+}
+
+r = requests.post(url, data=data)
+
+print(r.text)
+
+
+
